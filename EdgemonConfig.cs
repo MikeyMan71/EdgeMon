94107277b2ad058031babf3b7ce3a29a21597384
Michael Aigle <michael.aigle@web.de>
2024-12-29 20:39:03 +0100

    4.4.8.0

--- EdgemonConfig.cs
+++ EdgemonConfig.cs
@@ -19,6 +19,7 @@ namespace EdgeMon
         //MAMconfig.Config edgeconfig = new MAMconfig.Config("EdgeMon");
 
 
+
         public string TCP { get; set; }
         public int port { get; set; }
         public bool battery { get; set; }
